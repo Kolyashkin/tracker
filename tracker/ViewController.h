4282542bbabddec1fbefc8537e6c66f4c3ba2932
Kolyashkin <donnellnickolas@gmail.com>
2016-06-22 15:00:09 -0400

    Initial Commit

---
+++ tracker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  tracker
+//
+//  Created by Nickolas McDaniels on 6/22/16.
+//  Copyright © 2016 Nickolas Donnell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ tracker/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  tracker
+//
+//  Created by Nickolas McDaniels on 6/22/16.
+//  Copyright © 2016 Nickolas Donnell. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
